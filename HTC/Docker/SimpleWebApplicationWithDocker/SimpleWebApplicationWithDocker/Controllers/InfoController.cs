0921a9e9b4caaf12205d866b9b6a8a33082f3064
Maksim Golev <mgolev@htc-cs.ru>
2022-02-06 12:13:46 +0400

    Adding simple web application with docker.

---
+++ HTC/Docker/SimpleWebApplicationWithDocker/SimpleWebApplicationWithDocker/Controllers/InfoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace SimpleWebApplicationWithDocker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InfoController : ControllerBase
+    {
+        [HttpGet]
+        public string GetOSInfo()
+        {
+            string returnValue;
+
+            returnValue = Environment.OSVersion.ToString();
+
+            return returnValue;
+        }
+    }
+}
