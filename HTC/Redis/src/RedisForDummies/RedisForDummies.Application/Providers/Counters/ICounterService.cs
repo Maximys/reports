09862a6b4d28d190cefc59de7de1ba427abef30c
Maksim Golev <mgolev@htc-cs.ru>
2023-09-25 14:00:19 +0400

    Base research of using Redis.

---
+++ HTC/Redis/src/RedisForDummies/RedisForDummies.Application/Providers/Counters/ICounterService.cs
@@ -0,0 +1,15 @@
+﻿namespace RedisForDummies.Application.Providers.Counters
+{
+    /// <summary>
+    /// Контракт Сервиса Счетчиков.
+    /// </summary>
+    public interface ICounterService
+    {
+        /// <summary>
+        /// Получить значение Счетчика.
+        /// </summary>
+        /// <param name="key">Ключ Счетчика.</param>
+        /// <returns>Значение Счетчика.</returns>
+        Task<int> GetAsync(string key);
+    }
+}
